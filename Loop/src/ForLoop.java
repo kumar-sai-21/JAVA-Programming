aab94b10891b1fb6f4d80e8639703a4e95c1bc7b
Sai kumar Satapathy <saikumar212000@gmail.com>
2021-06-15 01:05:08 +0530

    Loops Added

---
+++ Loop/src/ForLoop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/*
+ * Print 1 to n number
+ */
+public class ForLoop {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input= new Scanner(System.in);
+		int n=input.nextInt();
+		for(int i=1;i<n;i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
