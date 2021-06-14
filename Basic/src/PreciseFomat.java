a4c738c954eaa9ed08cd1ab91f22d8cfbcd8745a
Sai kumar Satapathy <saikumar212000@gmail.com>
2021-06-15 00:46:55 +0530

    Precise Fomat added

---
+++ Basic/src/PreciseFomat.java
@@ -0,0 +1,19 @@
+/*
+ *  
+ */
+
+
+public class PreciseFomat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a=3.3;
+		double b=2.3;
+		double result = a/b;
+		System.out.println(result);
+        System.out.format("%.3f", result);
+
+
+	}
+
+}
