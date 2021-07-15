034cba872a538e4a218a912fa59dabce8fefec6c
Sai kumar Satapathy <saikumar212000@gmail.com>
2021-07-15 12:56:55 +0530

    collection

---
+++ Java-Collection/src/Arraylist/BasicArrList.java
@@ -0,0 +1,15 @@
+package Arraylist;
+
+import java.util.*;
+public class BasicArrList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String>list=new ArrayList<String>();
+		list.add("Sai");
+		list.add("kumar");
+		list.add("Satapathy");
+		System.out.println(list);
+	}
+
+}
